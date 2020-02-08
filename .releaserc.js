8e3ce301541f6a9a03093d5e5a0d540a2a9011e9
Oleg Pimenov <opimenov@ptsecurity.com>
2020-02-08 21:48:08 +0300

    First commit

---
+++ .releaserc.js
@@ -0,0 +1,17 @@
+{
+    "plugins": [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    { type: 'docs', scope: 'README', release: 'patch' },
+                    { type: 'minor', release: 'minor' },
+                    { type: 'major', release: 'major' },
+                    { type: 'patch', release: 'patch' },
+                    { scope: 'no-release', release: false },
+                ],
+            },
+        ],
+        '@semantic-release/release-notes-generator'
+    ]
+}
